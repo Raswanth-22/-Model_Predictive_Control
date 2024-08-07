51eae0c8933e62d6b1bc0e55818f842dd222621a
Raswanth Velmurugan <73633817+Raswanth-22@users.noreply.github.com>
2024-08-07 00:11:11 -0400

    Update MathFunctions.h

--- src/Eigen-3.3/Eigen/src/Core/arch/AVX/MathFunctions.h
+++ src/Eigen-3.3/Eigen/src/Core/arch/AVX/MathFunctions.h
@@ -1,11 +1,4 @@
-// This file is part of Eigen, a lightweight C++ template library
-// for linear algebra.
-//
-// Copyright (C) 2014 Pedro Gonnet (pedro.gonnet@gmail.com)
-//
-// This Source Code Form is subject to the terms of the Mozilla
-// Public License v. 2.0. If a copy of the MPL was not distributed
-// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 #ifndef EIGEN_MATH_FUNCTIONS_AVX_H
 #define EIGEN_MATH_FUNCTIONS_AVX_H
