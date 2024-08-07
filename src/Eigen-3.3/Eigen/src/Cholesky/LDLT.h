81a0205a0b3b9d9bec131aec7313958b93312baa
Raswanth Velmurugan <73633817+Raswanth-22@users.noreply.github.com>
2024-08-07 00:09:44 -0400

    Update LDLT.h

--- src/Eigen-3.3/Eigen/src/Cholesky/LDLT.h
+++ src/Eigen-3.3/Eigen/src/Cholesky/LDLT.h
@@ -1,14 +1,4 @@
-// This file is part of Eigen, a lightweight C++ template library
-// for linear algebra.
-//
-// Copyright (C) 2008-2011 Gael Guennebaud <gael.guennebaud@inria.fr>
-// Copyright (C) 2009 Keir Mierle <mierle@gmail.com>
-// Copyright (C) 2009 Benoit Jacob <jacob.benoit.1@gmail.com>
-// Copyright (C) 2011 Timothy E. Holy <tim.holy@gmail.com >
-//
-// This Source Code Form is subject to the terms of the Mozilla
-// Public License v. 2.0. If a copy of the MPL was not distributed
-// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 #ifndef EIGEN_LDLT_H
 #define EIGEN_LDLT_H
